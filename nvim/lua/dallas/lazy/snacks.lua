6ca2ec66ff8c17f0adf5b4beeeb7d50ccc2ef9ff
dallas <dallasmgere@gmail.com>
2025-07-29 19:49:00 -0500

    adding lazy git

--- nvim/lua/dallas/lazy/snacks.lua
+++ nvim/lua/dallas/lazy/snacks.lua
@@ -2,21 +2,14 @@ return {
     "folke/snacks.nvim",
     priority = 1000,
     lazy = false,
-    ---@type snacks.Config
     opts = {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
         bigfile = { enabled = true },
         dashboard = { enabled = true },
         explorer = { enabled = true },
         indent = { enabled = true },
         input = { enabled = true },
         picker = { enabled = true },
-        notifier = {
-            enabled = true,
-            timeout = 5000
-        },
+        notifier = { enabled = true, timeout = 5000 },
         quickfile = { enabled = true },
         scope = { enabled = true },
         scroll = { enabled = true },
@@ -24,4 +17,11 @@ return {
         words = { enabled = true },
         lazygit = { enabled = true }
     },
+    config = function(_, opts)
+        require("snacks").setup(opts)
+        vim.keymap.set('n', '<leader>lg', function()
+            require("snacks.lazygit").open()
+        end, { desc = "Open LazyGit via Snacks" })
+    end,
 }
+
